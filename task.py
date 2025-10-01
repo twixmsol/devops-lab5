317e2f3d002bacbd5d963ee9a6ba75e068555cc6
twixmsol <dilnazka20008@gmail.com>
2025-10-01 10:49:42 +0500

    Update task.py
    пайтон коды

--- task.py
+++ task.py
@@ -1,8 +1,8 @@
 #Лаб 5 пайтон
 with open ("myfile.txt","w") as f:
-  f.write("Тестовый\n")
-  f.write("Вторая строка\n")
+  f.write("Тест\n")
+  f.write("екінші жол\n")
 with open ("myfile.txt", "r") as f:
   content=f.read()
-print("Содержимое файла:")
+print("Файл құрылымы:")
 print(content)
